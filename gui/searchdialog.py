40d07ea82fca840a1aaa08f368baf5e6971b8eef
Italo Silva <italo@maleldil.com>
2021-03-27 20:58:52 -0300

    Add Enter for searching and make it possible to maximise window
    Code originally from https://github.com/rholi/Search/pull/5

--- gui/searchdialog.py
+++ gui/searchdialog.py
@@ -35,14 +35,14 @@ class SearchDialog(QDialog):
 		self.scheme = scheme
 
 		self.root_node = root_node
-		
+
 		self.counter = 0
 		self.counterFileInserted = 0
 		self.counterDirInserted = 0
-		
-		
+
+
 		self.searchStopEvent = Event()
-		
+
 		self.directoryDict = {}
 		self.fileNameQueue = queue.Queue()
 
@@ -50,86 +50,88 @@ class SearchDialog(QDialog):
 		self.fileNameQueueTimer.setInterval(500)
 		self.fileNameQueueTimer.timeout.connect(self.addItemsFromQueue)
 		self.fileNameQueueTimer.start()
-		
+
 		self.setAttribute(Qt.WA_DeleteOnClose)
 		self.title = 'Search'
-		
+
 		self.directory = directory
 		self.fileFilter = ''
 
 		self.setup = self.load_setup(SEARCHSETUP)
-		
+
 		self.initUI()
 		self.createUI()
 
 	def initUI(self):
 		self.resize(1000, 400)
 		self.setWindowTitle(self.title)
+		self.setWindowFlags(QtCore.Qt.Window)
 
 	def createUI(self):
 		self.layout = QVBoxLayout(self)
 		self.layout.setContentsMargins(0, 0, 0, 0)
-	
+
 		groupBox = QGroupBox("Search")
 		self.layout.addWidget(groupBox)
-		
+
 		gridLayout = QGridLayout()
 		groupBox.setLayout(gridLayout)
-		
+
 		self.searchSpotlightLabel = QLabel('search for files with spotlight')
 		self.searchSpotlightCheckBox = QCheckBox('Sp&otlight Search')
 		self.searchSpotlightCheckBox.stateChanged.connect(self.spotlightChecked)
-		
+
 		fileFilterLabel = QLabel('file filter')
 		self.fileFilterText = QComboBox()
 		self.fileFilterText.setEditable(True)
-		
+		self.fileFilterText.lineEdit().returnPressed.connect(self.searchButtonClicked)
+
 		searchDirLabel = QLabel('search in directory')
 		self.searchDirText = QLineEdit(self.directory)
-		
-		
+
+
 		subdirsHBoxLayout = QHBoxLayout(self)
-				
+
 		self.searchSubDirLabel = QLabel('search in subdirs:')
-		
+
 		self.searchSubDirAll = QRadioButton('all')
 		self.searchSubDirAll.setChecked(True)
-		
+
 		self.searchSubDirCurrent = QRadioButton('only current')
 		self.searchSubDirAll.setChecked(False)
-		
+
 		self.searchSubDirLevel = QRadioButton('depth:')
 		self.searchSubDirAll.setChecked(False)
-		
-		self.searchSubDirLevelText = QLineEdit('1')		
+
+		self.searchSubDirLevelText = QLineEdit('1')
 		self.searchSubDirLevelText.setInputMask('99')
-		
+
 		self.includeDirectoriesCheckBox = QCheckBox('include directories in fileslist')
-		
+
 		subdirsHBoxLayout.addWidget(self.includeDirectoriesCheckBox)
 		subdirsHBoxLayout.addStretch(1)
-		
+
 		subdirsHBoxLayout.addWidget(self.searchSubDirLabel)
-		
-		
+
+
 		# search depth
 		self.searchSubDirButtonGroup = QButtonGroup()
 		self.searchSubDirButtonGroup.addButton(self.searchSubDirAll,SEARCH_SUBDIR_MODE_ALL)
 		self.searchSubDirButtonGroup.addButton(self.searchSubDirCurrent,SEARCH_SUBDIR_MODE_CURRENT)
 		self.searchSubDirButtonGroup.addButton(self.searchSubDirLevel,SEARCH_SUBDIR_MODE_LEVEL)
-		
+
 		subdirsHBoxLayout.addWidget(self.searchSubDirAll)
 		subdirsHBoxLayout.addWidget(self.searchSubDirCurrent)
 		subdirsHBoxLayout.addWidget(self.searchSubDirLevel)
 		subdirsHBoxLayout.addWidget(self.searchSubDirLevelText)
 		subdirsHBoxLayout.addStretch(1)
-		
+
 		# search text
 		self.searchCheckBox = QCheckBox('search &text')
 		self.searchCheckBox.stateChanged.connect(self.searchTextChecked)
 		self.searchText = QComboBox()
 		self.searchText.setEditable(True)
-		
+
 		# encoding
 		self.encodingLabel = QLabel('encoding')
 		self.encodingText = QComboBox()
@@ -137,55 +139,55 @@ class SearchDialog(QDialog):
 		self.encodingText.addItem('utf-16')
 		self.encodingText.addItem('cp437')
 		self.encodingText.addItem('cp1252')
-		
+
 		self.encodingText.setEditable(True)
 
 		# buttons
 		self.cancelButton = QPushButton("&Cancel")
 		self.cancelButton.clicked.connect(self.closeDialog)
-		
+
 		self.searchButton = QPushButton("&Search")
 		self.searchButton.clicked.connect(self.searchButtonClicked)
-		
+
 		self.stopButton = QPushButton("Sto&p")
 		self.stopButton.clicked.connect(self.stopButtonClicked)
-		
+
 		self.showInFmanButton = QPushButton("show results in &fman pane")
 		self.showInFmanButton.clicked.connect(self.showInFman)
-		
-		
+
+
 		self.messageLabel = QLabel('')
-		
+
 		self.counterLabel = QLabel('')
 
 		gridLayout.addWidget(self.searchSpotlightCheckBox,0,0)
-		gridLayout.addWidget(self.searchSpotlightLabel,0,1)	
+		gridLayout.addWidget(self.searchSpotlightLabel,0,1)
 
-		gridLayout.addWidget(fileFilterLabel,1,0)	
+		gridLayout.addWidget(fileFilterLabel,1,0)
 		gridLayout.addWidget(self.fileFilterText,1,1)
-		
-		gridLayout.addWidget(searchDirLabel,2,0)	
+
+		gridLayout.addWidget(searchDirLabel,2,0)
 		gridLayout.addWidget(self.searchDirText,2,1)
-		
+
 		gridLayout.addLayout(subdirsHBoxLayout,3,1)
-		
+
 		gridLayout.addWidget(self.searchCheckBox,4,0)
 		gridLayout.addWidget(self.searchText,4,1)
-		
+
 		encodingBoxLayout = QHBoxLayout(self)
 		encodingBoxLayout.addWidget(self.encodingLabel)
 		encodingBoxLayout.addWidget(self.encodingText)
 		encodingBoxLayout.addStretch(1)
-		
+
 		gridLayout.addLayout(encodingBoxLayout,5,1)
-		
+
 		gridLayout.addWidget(self.counterLabel,6,0)
 		gridLayout.addWidget(self.messageLabel,6,1)
-	
+
 		self.searchResultList = QListWidget()
 		self.layout.addWidget(self.searchResultList)
 		self.searchResultList.itemDoubleClicked.connect(self.onDoubleClickSearchResultList)
-		
+
 		self.progressBar = QProgressBar(self)
 		self.layout.addWidget(self.progressBar)
 		self.progressBar.setProperty("value", 0)
@@ -196,13 +198,13 @@ class SearchDialog(QDialog):
 		buttonBoxLayout.addWidget(self.searchButton)
 		buttonBoxLayout.addWidget(self.stopButton)
 		buttonBoxLayout.addWidget(self.showInFmanButton)
-		
+
 		self.layout.addLayout(buttonBoxLayout)
-		
+
 		self.buttonMode(BUTTON_MODE_FOR_SEARCHING)
-		
+
 		self.preset()
-		
+
 
 	def preset(self):
 		# set from setup
@@ -211,7 +213,7 @@ class SearchDialog(QDialog):
 			self.searchSpotlightCheckBox.setChecked(spotlightcheck)
 		except Exception as e:
 			pass
-			
+
 		try:
 			filefilterhistory = self.setup['filefilterhistory']
 			self.fileFilterText.addItems(filefilterhistory)
@@ -241,81 +243,81 @@ class SearchDialog(QDialog):
 			self.searchCheckBox.setChecked(searchtextcheck)
 		except Exception as e:
 			pass
-			
+
 		try:
 			searchsubdirallcheck = self.setup['searchsubdirall']
 			self.searchSubDirAll.setChecked(searchsubdirallcheck)
 		except Exception as e:
 			pass
-			
+
 		try:
 			searchsubdircurrentcheck = self.setup['searchsubdircurrent']
 			self.searchSubDirCurrent.setChecked(searchsubdircurrentcheck)
 		except Exception as e:
 			pass
-			
+
 		try:
 			searchsubdirlevelcheck = self.setup['searchsubdirlevel']
 			self.searchSubDirLevel.setChecked(searchsubdirlevelcheck)
 		except Exception as e:
-			pass	
-			
+			pass
+
 		try:
 			searchsubdirleveltext = self.setup['searchsubdirleveltext']
 			self.searchSubDirLevelText.setText(searchsubdirleveltext)
 		except Exception as e:
-			pass	
+			pass
 
 		try:
 			includedirectoriescheck = self.setup['includedirectoriescheck']
 			self.includeDirectoriesCheckBox.setChecked(includedirectoriescheck)
 		except Exception as e:
-			pass	
-			
+			pass
+
 		try:
 			encoding = self.setup['encoding']
 			self.encodingText.setEditText(encoding)
 		except Exception as e:
-			pass	
-			
-			
+			pass
+
+
 		# spotlight only for Mac
 		if platform.system() == 'Darwin':
 			self.setSpotlightMode(self.searchSpotlightCheckBox.isChecked(),True)
 			self.setTextMode(self.searchCheckBox.isChecked(),True)
 		else:
-			# spotlight mode is off 
+			# spotlight mode is off
 			self.searchSpotlightCheckBox.setChecked(False)
 			self.setSpotlightMode(self.searchSpotlightCheckBox.isChecked(),False)
 			self.setTextMode(self.searchCheckBox.isChecked(),True)
-			
-		
-			
+
+
+
 	def spotlightChecked(self,checked):
 		# turn off text search if spotlight mode is on
 		if checked:
 			self.setTextMode(False,False)
-	
+
 		self.setSpotlightMode(checked,True)
-		
-		
+
+
 	def searchTextChecked(self,checked):
 		self.setTextMode(checked,True)
-		
-		
+
+
 	def setTextMode(self,enabled,visible=False):
 		self.searchCheckBox.setVisible(visible)
-		
+
 		self.searchText.setVisible(enabled and visible)
 
 		# is spotlight is active no encoding
 		if not self.searchSpotlightCheckBox.isChecked():
 			self.encodingLabel.setVisible(enabled and visible)
 			self.encodingText.setVisible(enabled and visible)
-		
-	
+
+
 	def setSpotlightMode(self,enabled,visible=False):
-		
+
 		self.searchSpotlightLabel.setVisible(visible)
 		self.searchSpotlightCheckBox.setVisible(visible)
 
@@ -325,15 +327,15 @@ class SearchDialog(QDialog):
 		self.searchSubDirCurrent.setVisible(not enabled)
 		self.searchSubDirLevel.setVisible(not enabled)
 		self.searchSubDirLevelText.setVisible(not enabled)
-		
+
 		self.searchCheckBox.setVisible(visible)
 		self.searchText.setVisible(visible)
-			
+
 		self.encodingLabel.setVisible(not enabled)
 		self.encodingText.setVisible(not enabled)
-		
+
 		self.setTextMode(self.searchCheckBox.isChecked(),True)
-		
+
 
 	def onDoubleClickSearchResultList(self,item):
 		fullname = item.text()
@@ -345,8 +347,8 @@ class SearchDialog(QDialog):
 
 			self.fman_pane.set_path(as_url(path),callback)
 
-	
-	def closeDialog(self):		
+
+	def closeDialog(self):
 
 		try:
 			self.fileNameQueueTimer.stop()
@@ -365,78 +367,78 @@ class SearchDialog(QDialog):
 		except Exception as e:
 			show_status_message('error: %s' %(e))
 
-		
-	def showModal(self):	
+
+	def showModal(self):
 		self.show()
 		rc = self.exec_()
-	
+
 	def keyPressEvent(self, e):
 		if e.key() == Qt.Key_Escape:
 			self.closeDialog()
-			
+
 	def buttonMode(self,button_mode):
 		try:
 			if button_mode == BUTTON_MODE_FOR_SEARCHING:
-				
+
 				self.stopButton.setEnabled(False)
 				self.stopButton.setVisible(False)
-				
+
 				self.searchButton.setVisible(True)
 				self.searchButton.setEnabled(True)
 				self.searchButton.setDefault(True)
-			
-			elif button_mode == BUTTON_MODE_FOR_STOPPING:			
-				
+
+			elif button_mode == BUTTON_MODE_FOR_STOPPING:
+
 				self.searchButton.setEnabled(False)
 				self.searchButton.setVisible(False)
-				
+
 				self.stopButton.setVisible(True)
 				self.stopButton.setEnabled(True)
 				self.stopButton.setDefault(True)
-				
+
 			QtWidgets.QApplication.instance().processEvents()
-			
+
 		except Exception as e:
 			show_status_message('error: %s' %(e))
-			
-		
+
+
 	def searchButtonClicked(self):
 		show_status_message('searching...')
 		self.save_setup(SEARCHSETUP)
 		self.buttonMode(BUTTON_MODE_FOR_STOPPING)
 		self.searchInDir()
-		
-	
+
+
 	def stopButtonClicked(self):
 		show_status_message('stopping...')
 		self.buttonMode(BUTTON_MODE_FOR_SEARCHING)
 		self.searchStop()
-		
+
 	def searchInDir(self):
 
 		try:
-		
+
 			if hasattr(self,'searchThread'):
 				self.searchThread.quit()
 				self.searchThread.wait()
-		
-		
+
+
 			pattern = self.fileFilterText.currentText()
 			if len(pattern) == 0:
 				pattern = '*'
-				
+
 			directory = self.searchDirText.text()
-		
+
 			searchText = ''
 			if self.searchCheckBox.isChecked():
 				searchText = self.searchText.currentText()
-		
+
 			searchSubDirMode = self.searchSubDirButtonGroup.checkedId()
 			searchSubDirLevelText = self.searchSubDirLevelText.text()
 			includeDirectories = self.includeDirectoriesCheckBox.isChecked()
-			
+
 			searchSubDirLevel = 0
-			
+
 			if searchSubDirMode == SEARCH_SUBDIR_MODE_ALL:
 				searchSubDirLevel = 9999999999999
 			elif searchSubDirMode == SEARCH_SUBDIR_MODE_CURRENT:
@@ -446,7 +448,7 @@ class SearchDialog(QDialog):
 					searchSubDirLevel = int(searchSubDirLevelText)
 				except Exception as e1:
 					searchSubDirLevel = 0
-			
+
 			self.fileNameQueue.queue.clear()
 			self.searchResultList.clear()
 			self.root_node.clear()
@@ -457,7 +459,7 @@ class SearchDialog(QDialog):
 
 			self.progress(0)
 			self.progressBar.setVisible(True)
-			
+
 			self.setFoundFiles(self.counter,self.counterFileInserted,self.counterDirInserted)
 
 			searchmode = SEARCH_MODE_SEQU
@@ -467,7 +469,7 @@ class SearchDialog(QDialog):
 			encoding = self.encodingText.currentText()
 			if len(encoding.strip()) == 0:
 				encoding = 'utf-8'
-				
+
 			self.searcher = Searcher(directory,pattern,searchText,self.searchStopEvent,searchmode,searchSubDirLevel,includeDirectories,encoding)
 			self.searchThread = QtCore.QThread()
 			self.searcher.moveToThread(self.searchThread)
@@ -481,24 +483,24 @@ class SearchDialog(QDialog):
 			self.searchThread.start()
 		except Exception as e:
 			show_status_message('error: %s' %(e))
-	
+
 
 	def searchStop(self):
 		self.searchStopEvent.set()
-		
+
 		if hasattr(self,'searchThread'):
 			self.searchThread.quit()
-			self.searchThread.wait()		
+			self.searchThread.wait()
 
 	def showInFman(self):
-		
+
 		if len(self.root_node.children) > 0:
 			scheme = 'search://'
 
 			fullname = self.searchDirText.text()
 			self.fman_pane.set_path(as_url(fullname,scheme))
-			
-			self.closeDialog()	
+
+			self.closeDialog()
 		else:
 			show_alert('no search results to display in fman!')
 
@@ -512,20 +514,20 @@ class SearchDialog(QDialog):
 		if filename.startswith('[D]'):
 			filename = filename[3:]
 			self.counterDirInserted += 1
-			
+
 		elif filename.startswith('[F]'):
 			filename = filename[3:]
 			self.counterFileInserted += 1
 		else:
 			self.counterFileInserted += 1
-			
+
 		self.searchResultList.addItem(filename)
 
 		if(self.counter > 0):
 			self.progress(100 / self.counter * (self.counterFileInserted + self.counterDirInserted))
-		
+
 	def addItems(self,filenames):
-	
+
 		newfilenames = []
 		for file in filenames:
 			filename = file
@@ -537,31 +539,31 @@ class SearchDialog(QDialog):
 				self.counterFileInserted += 1
 			else:
 				self.counterFileInserted += 1
-				
+
 			newfilenames.append(filename)
-	
+
 		self.searchResultList.addItems(newfilenames)
 
 		if(self.counter > 0):
 			self.progress(100 / self.counter * (self.counterFileInserted + self.counterDirInserted))
 
 		QtWidgets.QApplication.instance().processEvents()
-			
-		
-		
+
+
+
 	def setFoundFiles(self,counter,countFiles,countDirectories):
 		if countFiles == 1:
 			filestext = 'file'
 		else:
 			filestext = 'files'
-			
+
 		if countDirectories == 1:
 			dirtext = 'directory'
 		else:
 			dirtext = 'directories'
-			
+
 		self.counterLabel.setText('%i %s\n%i %s found' %(countFiles,filestext,countDirectories,dirtext))
-		
+
 	def addItemsFromQueue(self):
 		try:
 			self.setFoundFiles(self.counter,self.counterFileInserted,self.counterDirInserted)
@@ -572,7 +574,7 @@ class SearchDialog(QDialog):
 			while not self.fileNameQueue.empty() and self.fileNameQueueTimer.isActive():
 				filename = self.fileNameQueue.get()
 				stringList.append(filename)
-				
+
 				if len(stringList) > 1000:
 					self.addItems(stringList)
 					stringList.clear()
@@ -581,25 +583,25 @@ class SearchDialog(QDialog):
 				QtWidgets.QApplication.instance().processEvents()
 
 			self.addItems(stringList)
-			
-	
+
+
 		except Exception as e:
 			show_status_message('error: %s' %(e))
 
 	def searchResultAddItem(self,filename):
 		self.fileNameQueue.put(filename)
-		
+
 		if filename.startswith('[D]'):
 			filename = filename[3:]
 		elif filename.startswith('[F]'):
 			filename = filename[3:]
-			
+
 		self.root_node.add_from_os_path(filename)
-		
-		
+
+
 		self.counter += 1
-		
-		
+
+
 	def showMessage(self,message):
 		try:
 			self.messageLabel.setText(message)
@@ -610,8 +612,8 @@ class SearchDialog(QDialog):
 		self.buttonMode(BUTTON_MODE_FOR_SEARCHING)
 		self.showMessage('search finished - double click on entry to show file in fman pane')
 		clear_status_message()
-		
-		
+
+
 	def load_setup(self,setupfile):
 		list = {}
 
@@ -624,39 +626,39 @@ class SearchDialog(QDialog):
 
 	def save_setup(self,setupfile):
 		setup = {}
-		
+
 		filefilter = self.fileFilterText.currentText()
-		
+
 		# save only the last 10 entries
 		filefilterhistory = []
 		try:
 			filefilterhistory = self.setup['filefilterhistory']
 		except Exception as e:
 			pass
-		
+
 		if not filefilter in filefilterhistory:
 			filefilterhistory.insert(0,filefilter)
-			
+
 		if len(filefilterhistory) > 10:
 			filefilterhistory = filefilterhistory[:10]
-		
-		
+
+
 		searchtext = self.searchText.currentText()
-		
+
 		# save only the last 10 entries
 		searchtexthistory = []
 		try:
 			searchtexthistory = self.setup['searchtexthistory']
 		except Exception as e:
 			pass
-		
+
 		if not searchtext in searchtexthistory:
 			searchtexthistory.insert(0,searchtext)
-			
+
 		if len(searchtexthistory) > 10:
 			searchtexthistory = searchtexthistory[:10]
-		
-		
+
+
 		setup['filefilter'] 			 = filefilter
 		setup['filefilterhistory'] 		 = filefilterhistory
 		setup['searchtext'] 			 = searchtext
@@ -669,6 +671,6 @@ class SearchDialog(QDialog):
 		setup['searchsubdirleveltext'] 	 = self.searchSubDirLevelText.text()
 		setup['includedirectoriescheck'] = self.includeDirectoriesCheckBox.checkState()
 		setup['encoding']             	 = self.encodingText.currentText()
-		
+
 		with open(setupfile, 'w') as outfile:
 			json.dump(setup, outfile)
